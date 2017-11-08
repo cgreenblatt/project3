a457fa631bdcaf144c46c92d233059146390aed8
Cynthia Greenblatt <cynthia_greenblatt@yahoo.com>
2017-11-08 11:10:49 -0800

    feat: Add makeGrid function

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
 // Select color input
 // Select size input
+let buttn = $( "input[type=submit]" );
+buttn.click(function(event) {
+    event.preventDefault();
+    let rows = $( "input[name=height" ).val();
+    let cols = $( "input[name=width" ).val();
+    makeGrid(rows,cols);
+});
 
-// When size is submitted by the user, call makeGrid()
+function makeGrid(rows, cols) {
 
-function makeGrid() {
-
-// Your code goes here!
+    let tableStr = "";
+    $( "#pixel_canvas" ).children().remove();
+    for (let i = 0; i < rows; i++) {
+        tableStr += "<tr>";
+        for (let j = 0; j < cols; j++) {
+            tableStr += "<td></td>"
+        }
+        tableStr += "</tr>";
+    }
+    $( "#pixel_canvas" ).append(tableStr);
 
 }
